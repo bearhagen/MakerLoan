6a67f1e01e4025c03e152a5a752fdae844ce4d91
Bear Hagen <post@bearhagen.com>
2017-03-06 08:28:42 +0100

    Variables for branding colors

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -22,6 +22,25 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		Color colorRed         = new Color(194, 111, 125);
+		Color colorMagenta     = new Color(163, 150, 163);
+		Color colorBlue        = new Color(111, 167, 194);
+		Color colorGreen       = new Color(111, 194, 180);
+		Color colorGreenDark   = new Color(52, 126, 132);
+		Color colorYellow      = new Color(194, 180, 111);
+		Color colorOrange      = new Color(215, 120, 105);
+		Color colorWhite       = new Color(255, 255, 255);
+		Color colorBlack       = new Color(35, 32, 31);
+		Color colorGreyLight   = new Color(236, 234, 229);   Color colorGrayLight = colorGreyLight;
+		Color colorGrey        = new Color(200, 194, 190);   Color colorGray      = colorGrey;
+		Color colorPrimary     = colorGreen; 
+		Color colorPrimaryDark = colorGreenDark;
+		Color colorSecondary   = colorOrange;
+		Color colorTertiary    = colorMagenta;
+		Color colorSuccess     = colorGreen;
+		Color colorInfo        = colorBlue;
+		Color colorAlert       = colorRed;
+		
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
