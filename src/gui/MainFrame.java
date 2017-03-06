04d654df9fe622359673d3d9a9a187a72eed2e41
Bear Hagen <post@bearhagen.com>
2017-03-06 08:27:53 +0100

    Made window start

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,31 @@
+package gui;
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+}
