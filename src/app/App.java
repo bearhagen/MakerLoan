103919b7c1b34ca2c4eefa8ce26f187b474efbe6
Bear Hagen <post@bearhagen.com>
2017-03-06 11:16:39 +0100

    No need to submit student id anymore

--- src/app/App.java
+++ src/app/App.java
@@ -12,15 +12,19 @@ import inventory.Loan;
 
 public class App {
 	public static void main(String[] args) {
+		Department larerStudie = new Department("Avdeling for lærerutdanning", "LA123");
+		new Course("Engelsk", "LAENG123", larerStudie);
+		
 		Department it = new Department("Avdeling for informasjonsteknologi", "IT123");
 		Course oop = new Course("Objektorientert programmering", "idk1234", it);
 		Course datatek = new Course("Datateknikk", "idk1235", it);
+		
 		ArrayList<Course> olasKurs = new ArrayList<Course>();
 		olasKurs.add(oop);
 		olasKurs.add(datatek);
 		
-		Student ola = new Student("Ola", "Nordmann", "12345678", it, olasKurs, "000001");
-		Student kari = new Student("Kari", "Nordmann", "12345678", it, olasKurs, "000002");
+		Student ola = new Student("Ola", "Nordmann", "12345678", it, olasKurs);
+		Student kari = new Student("Kari", "Nordmann", "12345678", it, olasKurs);
 		Item laptop = new Item("Dell XPS 13", 00000001);
 		Item mobil = new Item("iPhone 7", 00000002);		
 		
