51d1744e25ce4fc1233447f379f647d610447923
Bear Hagen <post@bearhagen.com>
2017-03-20 12:28:55 +0100

    Added toString, toStrings and exsist methods to item

--- src/inventory/Item.java
+++ src/inventory/Item.java
@@ -2,15 +2,15 @@ package inventory;
 
 import java.util.ArrayList;
 
+import hr.Person;
+
 public class Item {
 	private String name;
 	private int itemID;
-	private int amount;
 	private static ArrayList<Item> items = new ArrayList<Item>();
 	
 	public Item(String name, int itemID) {
 		this.name = name;
-		this.amount = amount;
 		this.itemID = itemID;
 		items.add(this);
 	}
@@ -30,16 +30,42 @@ public class Item {
 	public void setItemID(int itemID) {
 		this.itemID = itemID;
 	}
-
-	public int getAmount() {
-		return amount;
-	}
-
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
 	
 	public static ArrayList<Item> getItems() {
 		return items;
+	}
+	
+	/**
+	 *  Check if this loan is already created
+	 */
+	public static boolean exist(int itemID) {
+		for (int i = 0; i < items.size(); i++) {
+			Item tmpItem = items.get(i);
+			if	(tmpItem.getItemID() == itemID) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+		
+	/**
+	 *  Get values and turn them into a human friendly string
+	 */
+	@Override
+	public String toString() {
+		return itemID + " - " + name;
+	}
+	
+	/**
+	 *  Runs toString method on all the items in the item list and returns them in an array
+	 */
+	public static Object[] toStrings() {
+		ArrayList<String> strings = new ArrayList<String>();
+		for (int i = 0; i < items.size(); i++) {
+			strings.add(items.get(i).toString());
+		}
+		
+		return strings.toArray();
 	}
 }
